e4ec8e495bfb7181d07c9d2e5aedea5e7ff49ec6
yosemitii <lubin.suen@gmail.com>
2023-02-09 22:16:58 -0500

    make only one file

--- lab1/linker.cpp
+++ lab1/linker.cpp
@@ -5,8 +5,6 @@
 #include <map>
 #include <string.h>
 #include <iomanip>
-#include "Module.cpp"
-#include "Token.cpp"
 
 using namespace std;
 
@@ -14,20 +12,113 @@ using namespace std;
 #define USE_LIST 1;
 #define PROGRAM_TEXT 2;
 
+class Token
+{
+    int lineNum;
+    int lineOffset;
+    string token;
+
+public:
+    Token(int ln, int lo, string tok)
+    {
+        lineNum = ln;
+        lineOffset = lo;
+        token = tok;
+    }
+
+    int getLineNum()
+    {
+        return lineNum;
+    }
+
+    int getLineOffset()
+    {
+        return lineOffset;
+    }
+
+    string getToken()
+    {
+        return token;
+    }
+};
+
+
+class OpPair
+{
+public:
+    string op;
+    int addr;
+    OpPair(string o, int ad) {
+        op = o;
+        addr = ad;
+    }
+};
+
+class Module
+{
+private:
+    int offset;
+    vector<string> useList;
+    vector<OpPair> progText;
+
+public:
+    Module()
+    {}
+
+    int getOffset()
+    {
+        return offset;
+    }
+
+    void setOffset(int os)
+    {
+        offset = os;
+    }
+
+    vector<string> getUseList()
+    {
+        return useList;
+    }
+
+    void setUseList(string symbol)
+    {
+        useList.push_back(symbol);
+    }
+
+    vector<OpPair> getProgText() {
+        return progText;
+    }
+
+    void setProgText(string op, int addr) {
+        OpPair toAdd = OpPair(op, addr);
+        progText.push_back(toAdd);
+    }
+
+    void toString() {
+        cout << "--- Module info: ---" << endl;
+        cout << "Offset: " << offset << endl;
+        cout << "Use List: ";
+        for (string& s: useList) {
+            cout << s << " ";
+        }
+        cout << endl;
+        cout << "Prog Text: " << endl;
+        for (OpPair& opp: progText) {
+            cout << opp.op << " " << opp.addr << endl;
+        }
+    }
+};
 
 static vector<Token> tokenList;
 static std::map<string, int> symbolTable;
 vector<Module> moduleList;
 static int offset = 0;
-
 const char delim[5] = "\t 	 ";
 
 bool syntaxVerify(string line, int index);
 string convertToString(char *a, int size);
-int passOne(string line, int type);
-void __parseerror(int linenum, int lineoffset, int errcode);
 void tokenizer(string line, int lineNum);
-void passOneV2();
+void passOne();
 void passTwo();
 bool isNumber(const string &s);
 
@@ -66,7 +157,7 @@ int main(int argc, char **argv)
     // }
     inFile.close();
 
-    passOneV2();
+    passOne();
     cout << endl;
     cout << "====== after pass one =======" << endl;
     for (Module &m : moduleList)
@@ -106,7 +197,7 @@ void tokenizer(string line, int lineNum)
     }
 }
 
-void passOneV2()
+void passOne()
 {
     int tokenPairNum = 0;
     int index = 0;
@@ -258,112 +349,6 @@ void passTwo()
     }
 }
 
-int passOne(string line, int type, int tokenLeft)
-{
-    // Issues: str tok will get segmentation fault
-    // char *lineArr = new char[line.length() + 1];
-
-    // Issues: use char lineArr[] and the issue stays
-    // char lineArr[line.length() + 1];
-    char *lineArr = new char[line.length() + 1];
-    strcpy(lineArr, line.c_str());
-    int lineOffset = 1;
-    // cout << sizeof(lineArr) / sizeof(char) << endl;
-    // char *token = strtok(lineArr, " ");
-    // cout << "Token: " << token << endl;
-
-    char *token;
-    // const char delim[5] = "\t 	 ";
-    if (type % 3 == 0)
-    {
-        int count = 0;
-        string symbol;
-        int symbolVal;
-        int totalPairs;
-        while ((token = strsep(&lineArr, delim)) != NULL)
-        {
-            cout << "line: " << type << "Token: " << token << " size: " << sizeof(char) << endl;
-            if (count == 0)
-            {
-                totalPairs = stoi(token);
-            }
-            else if (count % 2 == 1)
-            {
-                symbol = token;
-                // if (symbolTable[symbol] != NULL) {
-                //     return false;
-                //     __parseerror(lineNum, lineOffset, 0);
-                // }
-            }
-            else
-            {
-                // Define a symbol
-                // Error 2: symbol multiple definition
-                symbolVal = stoi(token) + offset;
-                symbolTable[symbol] = symbolVal;
-            }
-            count++;
-            lineOffset += sizeof(token) / sizeof(char *);
-        }
-        // Error 1: symbol number mismatch
-        // Verify: total pair number matches or not
-        if (totalPairs != (count - 1) / 2)
-        {
-            // __parseerror(type, lineOffset, 0);
-            // return false;
-        }
-        else
-        {
-            for (const auto &kv : symbolTable)
-            {
-                cout << kv.first << "=" << kv.second << endl;
-            }
-        }
-    }
-    else if (type % 3 == 1)
-    {
-        while ((token = strsep(&lineArr, " ")) != NULL)
-        {
-            cout << "Token: " << token << endl;
-        }
-    }
-    else
-    {
-        if ((token = strsep(&lineArr, " ")) != NULL)
-        {
-            offset += stoi(token);
-            cout << "new offset: " << offset << endl;
-        }
-    }
-    return true;
-
-    // while (token != NULL)
-    // {
-    //     token = strtok(NULL, " ");
-    //     cout << "token: " << token << endl;
-    // }
-    // cout << "lineArr[]" << lineArr << "token:" << token << endl;
-}
-
-bool syntaxVerify(string line, int index)
-{
-
-    int length = line.length();
-    char *lineArr = new char[length + 1];
-    strcpy(lineArr, line.c_str());
-
-    vector<char *> tokenVector;
-    char *token = strtok(lineArr, " \0");
-    int num = stoi(token);
-    cout << "token: " << token << endl;
-    while (token != NULL)
-    {
-        token = strtok(NULL, " \n");
-        cout << "token: " << token << endl;
-    }
-    cout << "lineArr[]" << lineArr << "token:" << token << endl;
-    return true;
-}
 
 string convertToString(char *a, int size)
 {
@@ -385,16 +370,3 @@ bool isNumber(const string &s)
 }
 
 
-
-void __parseerror(int linenum, int lineoffset, int errcode)
-{
-    static char *errstr[] = {
-        "NUM_EXPECTED",           // Number expect, anything >= 2^30 is not a number either
-        "SYM_EXPECTED",           // Symbol Expected
-        "ADDR_EXPECTED",          // Addressing Expected which is A/E/I/R
-        "SYM_TOO_LONG",           // Symbol Name is too long
-        "TOO_MANY_DEF_IN_MODULE", // > 16
-        "TOO_MANY_USE_IN_MODULE", // > 16
-        "TOO_MANY_INSTR”, // total num_instr exceeds memory size (512)"};
-    printf("Parse Error line %d offset %d: %s\n", linenum, lineoffset, errstr[errcode]);
-}
